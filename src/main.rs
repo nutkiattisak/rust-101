749282fbf8996dd06fa5b7b1169a6c2f97b5984a
Kiattisak Chantharamaneechote <32291012+nutkiattisak@users.noreply.github.com>
2024-02-15 21:27:46 +0700

    feat: update function example

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,12 @@ fn main() {
     let a: i32 = 1;
     let b: i32 = 2;
 
+    let result = add(a, b);
+
     println!("Hello, world!");
-    println!("{} + {} = {}", a, b, a + b);
+    println!("{} + {} = {}", a, b, result);
+}
+
+fn add(a : i32, b : i32) -> i32 {
+    a + b
 }
